9673a52bb57b902a00cd29e6314812dd62bb123b
bengyee <bengyee94@gmail.com>
2021-07-19 20:58:50 +0800

    TIP Calculator App
    Changes
    ==========
    - Re-adjust the padding for radio and input people
    - Refactor the js into each component
    - Import scss into App.js instead of html
    - Import .svg into each component
    - Move images folder & compiled css into new Public folder
    - Run npm build and deploy the github page

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
 const path = require('path');
 const HWP = require('html-webpack-plugin');
 module.exports = {
-   entry: path.join(__dirname, './src/js/index.js'),
-   output: {
-       filename: 'build.js',
-       path: path.join(__dirname, './dist')},
-   module:{
-       rules:[{
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-       }]
-   },
-   plugins:[
-       new HWP(
-          {template: path.join(__dirname, './src/index.html')}
-       )
-   ]
+    entry: path.join(__dirname, './src/js/index.js'),
+    output: {
+        filename: 'build.js',
+        path: path.join(__dirname, './public')
+    },
+    module:{
+        rules:[
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: 'babel-loader'
+            },
+            {
+              test: /\.(css)$/,
+              exclude: /node_modules/,
+              use: ['style-loader','css-loader']
+            },
+            {
+              test: /\.(woff|woff2|eot|ttf|svg|jpg|png)$/,
+              use: {
+                loader: 'url-loader',
+              }
+            }
+          ]
+    },
+    plugins:[
+        new HWP(
+            {template: path.join(__dirname, './src/index.html')}
+        )
+    ]
 }
